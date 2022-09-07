d93a3b3ab956fcf4f30326b4bf18002aba23a758
Alejandro Morales <a.moralesc@javeriana.edu.co>
2022-09-06 19:20:52 -0500

    Creates values, basic tile class

--- flow.py
+++ flow.py
@@ -1,6 +1,6 @@
-from curses.ascii import SP
 import sys, os
 import pygame as pg
+from values import *
 
 pg.init()
 
@@ -10,36 +10,15 @@ pg.init()
 BASE_DIR = os.path.split(os.path.abspath(__file__))[0]
 DATA_DIR = os.path.join(BASE_DIR, "data")
 ASSETS_DIR = os.path.join(BASE_DIR, "assets")
-SPRITES_DIR = os.path.join(ASSETS_DIR, "sprites")
-
-# Colors
-COLORS = {
-    "white": (255, 255, 255),
-    "silver": (192, 192, 192),
-    "gray": (128, 128, 128),
-    "red": (255, 0, 0),
-    "orange": (255, 165, 0),
-    "moccasin": (255, 228, 181),
-    "green": (0, 128, 0),
-    "lime": (0, 255, 0),
-    "yellow": (255, 255, 0),
-    "purple": (128, 0, 128),
-    "magenta": (255, 0, 255),
-    "navy": (0, 0, 128),
-    "blue": (0, 0, 255),
-    "teal": (0, 128, 128),
-    "cyan": (0, 255, 255),
-    "maroon": (128, 0, 0),
-}
 
 # Tiles configuration
 TILE_SIZE = 48
-MAX_TILES_WIDTH = 15
-MAX_TILES_HEIGHT = 15
 
 # Display initialization
-DISPLAY_WIDTH = TILE_SIZE * MAX_TILES_WIDTH
-DISPLAY_HEIGHT = TILE_SIZE * MAX_TILES_HEIGHT
+DISPLAY_WIDTH = TILE_SIZE * MAX_GRID_N
+DISPLAY_HEIGHT = TILE_SIZE * MAX_GRID_N
+
+FPS = 30
 
 window = pg.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pg.display.set_caption("Flow")
@@ -49,13 +28,13 @@ timer = pg.time.Clock()
 # IMPORT ASSETS
 
 # Load sprites
-def load_image(name: str, colorkey: pg._common._ColorValue = None) -> pg.Surface:
-    """Loads an image from the assetss directory and returns it as a Surface object
+def load_image(file: str, scale: tuple = None) -> pg.Surface:
+    """Loads an image from the assets directory and returns it as a Surface object
 
     Args:
-        name (str): path to the image file
-        colorkey (pg._common._ColorValue, optional): transparent color to be placed
-        on top of the image. Defaults to None.
+        file (str): relative path from ASSETS_DIR to the image file
+        scale (tuple, optional): scaling in (width px, heigh px) to be done to the
+        image. Defaults to None.
 
     Raises:
         SystemExit: if the image file is not found or cannot be loaded
@@ -64,52 +43,81 @@ def load_image(name: str, colorkey: pg._common._ColorValue = None) -> pg.Surface
         pg.Surface: the image as a Surface object
     """
 
-    fullname = os.path.join(ASSETS_DIR, name)
     try:
-        image = pg.image.load(fullname)
+        file = os.path.join(ASSETS_DIR, file)
+        surface = pg.image.load(file)
     except (pg.error, FileNotFoundError) as message:
-        print("Cannot load image:", name)
+        print("Cannot load image:", file)
         raise SystemExit(message)
-    image = image.convert()
-    if colorkey is not None:
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey, pg.RLEACCEL)
-    return image
+    if scale:
+        surface = pg.transform.scale(surface, scale)
+    return surface.convert()
 
 
-TILE_STATES = {
-    [0, 0]: "start",
-}
-tiles = dict()
+TILE_STATES = [
+    (0, 0),  # "start",
+    (0, 1),  # "start_to_up",
+    (0, 2),  # "start_to_right",
+    (0, 3),  # "start_to_down",
+    (0, 4),  # "start_to_left",
+    (1, 3),  # "vertical",
+    (2, 4),  # "horizontal",
+    (1, 2),  # "up_to_right",
+    (2, 3),  # "right_to_down",
+    (3, 4),  # "down_to_left",
+    (4, 1),  # "left_to_up",
+    (1, 5),  # "up_to_middle",
+    (2, 5),  # "right_to_middle",
+    (3, 5),  # "down_to_middle",
+    (4, 5),  # "left_to_middle",
+]
 
 # Import tiles sprites
+tiles = dict()
 for color in COLORS:
-    tiles[color] = []
-    for tile in os.listdir(os.path.join(SPRITES_DIR, "tiles", color)):
-        tiles[color] += [load_image(os.path.join(SPRITES_DIR, "tiles", color, tile))]
+    tiles[color] = dict()
+    is_placed_state = {0: "", 1: "_placed"}
+
+    for state in TILE_STATES:
+        for is_placed, is_placed_str in is_placed_state.items():
+            tiles[color][(state[0], state[1], is_placed)] = load_image(
+                os.path.join(
+                    "sprites",
+                    "tiles",
+                    color,
+                    f"tile_{state[0]}{state[1]}{is_placed_str}_{color}.png",
+                )
+            )
 # Empty tile
-tiles["empty"] = [load_image(os.path.join(SPRITES_DIR, "tiles", "tile_empty.png"))]
+TILE_STATES += [(-1, -1)]
+tiles["empty"] = {
+    (-1, -1, 0): load_image(os.path.join("sprites", "tiles", "tile_empty.png")),
+    (-1, -1, 1): load_image(os.path.join("sprites", "tiles", "tile_empty.png")),
+}
 
 
 class Tile(pg.sprite.Sprite):
-    def __init__(self, x: int, y: int, color: str = None):
+    def __init__(self, row: int, col: int, color: str = None):
         super().__init__()
 
+        self.row = row
+        self.col = col
+
+        self.state = (0, 0, 0)
         self.is_start = True
+        self.is_placed = False
         self.is_path = False
 
-        if color is None:
+        if color is None or color == "empty":
             color = "empty"
             self.is_empty = True
             self.is_start = False
-
         self.color = color
 
-        self.image = tiles[color][0]
+        self.image = tiles[self.color][self.state]
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.x = col * TILE_SIZE
+        self.rect.y = row * TILE_SIZE
 
     def update(self):
         pass
---
+++ values.py
@@ -0,0 +1,26 @@
+# Colors
+COLORS = {
+    "white": (255, 255, 255),
+    "silver": (192, 192, 192),
+    "gray": (128, 128, 128),
+    "red": (255, 0, 0),
+    "orange": (255, 165, 0),
+    "moccasin": (255, 228, 181),
+    "green": (0, 128, 0),
+    "lime": (0, 255, 0),
+    "yellow": (255, 255, 0),
+    "purple": (128, 0, 128),
+    "magenta": (255, 0, 255),
+    "navy": (0, 0, 128),
+    "blue": (0, 0, 255),
+    "teal": (0, 128, 128),
+    "cyan": (0, 255, 255),
+    "maroon": (128, 0, 0),
+}
+
+# Points
+MIN_POINTS = 2
+MAX_POINTS = len(COLORS)
+
+# Grid size
+MAX_GRID_N = 15  # 15x15 is the maximum grid size
